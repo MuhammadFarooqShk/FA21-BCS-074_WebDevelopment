6e3667990bef5626d0833fde92c3bda616345d5e
Muhammad Farooq Sheikh <muhammadfarooqshk@gmail.com>
2024-05-22 09:54:17 +0500

    lab task 4 almost done

---
+++ LabTask4_Authentication/models/user.js
@@ -0,0 +1,26 @@
+mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+let User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ LabTask4_Authentication/routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const authmid=require('../middlewares/auth')
+const midware=require('../middlewares/main-middleware')
+/* GET home page. */
+router.get('/',midware,authmid, function(req, res, next) {
+  res.render('landingpage');
+});
+
+router.get('/home',authmid,function(req,res){
+  res.render('landingpage')
+})
+
+router.get('/contact-us',authmid ,function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/api-example',authmid ,function(req, res, next) {
+  res.render('api');
+});
+
+router.get('/product',authmid, function(req, res, next) {
+  res.render('product',{items: global.items});
+});
+
+router.get('/about',authmid, function(req, res, next) {
+  res.render('about');
+});
+// added
+router.get('/landingpage', authmid, function(req, res, next) {
+  res.render('landingpage', { items: global.items });
+});
+
+
+
+router.post("/package", (req, res) => {
+  try {
+    res.send(global.items);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+router.get("/package", (req, res) => {
+  try {
+    res.send(global.items);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+module.exports = router;
